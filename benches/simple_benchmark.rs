6cf5be3ed90a759e85cc9e251d1a053fb93bd01c
Pierre Marijon <pierre.marijon-ext@aphp.fr>
2022-04-22 12:53:53 +0200

    Reverse complement benchmark run only reverse complement

--- benches/simple_benchmark.rs
+++ benches/simple_benchmark.rs
@@ -33,23 +33,31 @@ pub fn compute_xor10(b: &[u8]) -> u64 {
         .sum()
 }
 
-pub fn rc_naive(b: &[u8]) -> u64 {
-    b.windows(K)
-        .map(|x| {
-            let k = naive_impl::Kmer::from(x);
+pub fn rc_naive_prepare(b: &[u8]) -> Vec<naive_impl::Kmer> {
+    b.windows(K).map(naive_impl::Kmer::from).collect()
+}
+
+pub fn rc_naive(b: &[naive_impl::Kmer]) -> u64 {
+    b.iter()
+        .map(|k| {
             k.to_reverse_complement();
             k.into_u64()
         })
         .sum::<u64>()
 }
 
-pub fn rc_xor10(b: &[u8]) -> u64 {
+pub fn rc_xor10_prepare(b: &[u8]) -> Vec<[u64; 1]> {
     let enc = kmers::encoding::xor10::Xor10;
 
-    b.windows(K)
-        .map(|x| {
-            let k: [u64; 1] = enc.encode(x);
-            enc.rev_comp::<K>(k);
+    b.windows(K).map(|x| enc.encode(x)).collect()
+}
+
+pub fn rc_xor10(b: &[[u64; 1]]) -> u64 {
+    let enc = kmers::encoding::xor10::Xor10;
+
+    b.iter()
+        .map(|k| {
+            enc.rev_comp::<K>(*k);
             k[0]
         })
         .sum::<u64>()
@@ -87,15 +95,22 @@ pub fn reverse_complement(c: &mut Criterion) {
         let input = generate(1 << i, charset);
         let bytes = input.as_bytes();
 
-        g.bench_with_input(BenchmarkId::new("naive", 1 << i), &bytes, |b, &s| {
-            b.iter(|| black_box(rc_naive(s)));
-        });
+        let naive_kmers = rc_naive_prepare(bytes);
+        let xor10_kmers = rc_xor10_prepare(bytes);
+
+        g.bench_with_input(
+            BenchmarkId::new("naive", 1 << i),
+            &naive_kmers,
+            |b, kmers| {
+                b.iter(|| black_box(rc_naive(kmers)));
+            },
+        );
 
         g.bench_with_input(
             BenchmarkId::new("kme.rs::xor10", 1 << i),
-            &bytes,
-            |b, &s| {
-                b.iter(|| black_box(rc_xor10(s)));
+            &xor10_kmers,
+            |b, kmers| {
+                b.iter(|| black_box(rc_xor10(kmers)));
             },
         );
     }
